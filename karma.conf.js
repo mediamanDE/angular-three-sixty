cde725ede5a7bbcfbe2d46059dfc769531f3ac1e
Marcel Kempf <marcel.kempf93@gmail.com>
2018-01-19 13:08:55 +0100

    Add travis configuration

--- karma.conf.js
+++ karma.conf.js
@@ -122,7 +122,23 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+        browsers: [
+            'ChromeTravisCi'
+        ],
+
+        customLaunchers: {
+            ChromeTravisCi: {
+                base: 'Chrome',
+                flags: [
+                    '--no-sandbox',
+                    // See https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md
+                    '--headless',
+                    '--disable-gpu',
+                    // Without a remote debugging port, Google Chrome exits immediately.
+                    '--remote-debugging-port=9222'
+                ]
+            }
+        },
 
 
         // Continuous Integration mode
